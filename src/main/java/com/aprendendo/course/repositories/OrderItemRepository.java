915bb110690bc040a6f3e2f330a1cfeee4177d0f
Daniel <daniel.nascimento@sette.ag>
2024-09-24 16:28:08 -0300

    OrderItem pt.2

---
+++ src/main/java/com/aprendendo/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.aprendendo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aprendendo.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
