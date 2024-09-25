e1b8a6734df56dc6dee84e9e2bb6e20b58502333
Daniel <daniel.nascimento@sette.ag>
2024-09-25 09:30:45 -0300

    Payment, one-to-one association

--- src/main/java/com/aprendendo/course/entities/Order.java
+++ src/main/java/com/aprendendo/course/entities/Order.java
@@ -9,13 +9,16 @@ import java.util.Set;
 import com.aprendendo.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -38,6 +41,8 @@ public class Order implements Serializable {
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL.ALL)
+	private Payment payment;
 	
 	public Order() {
 		
@@ -85,9 +90,16 @@ public class Order implements Serializable {
 		}
 	}
 	
-	
 	public Set<OrderItem> getItems(){
 		return items;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	@Override
