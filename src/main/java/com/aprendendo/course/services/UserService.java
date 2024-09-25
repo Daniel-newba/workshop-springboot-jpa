5956ad6cbff67f1d4078024482f2e1a9dc88397c
Daniel <daniel.nascimento@sette.ag>
2024-09-25 16:26:24 -0300

    user insert

--- src/main/java/com/aprendendo/course/services/UserService.java
+++ src/main/java/com/aprendendo/course/services/UserService.java
@@ -12,16 +12,23 @@ import com.aprendendo.course.repositories.UserRepository;
 @Component
 public class UserService {
 	
+	//
 	@Autowired
 	private UserRepository repository;
 	public List<User> findAll(){
 		return repository.findAll();
 	}
 	
+	//
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert (User obj) {
+		return repository.save(obj);
+	}
+	
 		
 	}
 
